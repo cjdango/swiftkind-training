212d2ba762b9bc4b0151111bb3182330270ca0ff
cjdango <cjdango@gmail.com>
2019-04-01 16:06:15 +0800

    Update assignment operators

--- assignment_operators.py
+++ assignment_operators.py
@@ -1,15 +1,28 @@
-dog_name = 'Jonathan'
-dog_age = 3
-dog_toy_count = 4
-dog_friends_count = 10
-dog_treats_count = 24
-dog_sibling_count = 2
-dog_son_count = 10
+def get_dog_name(name):
+    dog_name = name
+    dog_name += ':)'
+    return dog_name
 
-dog_name += ':)'
-dog_age -= 1
-dog_toy_count *= 4
-dog_friends_count /= 2
-dog_treats_count %= 5
-dog_sibling_count **= 2
-dog_son_count //= 3
+def get_dog_age(dog_age):
+    dog_age -= 1
+    return dog_age
+
+def get_dog_toy_count(dog_toy_count):
+    dog_toy_count *= 4
+    return dog_toy_count
+
+def get_dog_friends_count(dog_friends_count):
+    dog_friends_count /= 2
+    return dog_friends_count
+
+def get_dog_treats_count(dog_treats_count):
+    dog_treats_count %= 5
+    return dog_treats_count
+
+def get_dog_sibling_count(dog_sibling_count):
+    dog_sibling_count **= 2
+    return dog_sibling_count
+
+def get_dog_son_count(dog_son_count):
+    dog_son_count //= 3
+    return dog_son_count
