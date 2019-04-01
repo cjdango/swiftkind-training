b4f5356b1732c1057763f5d726a428e33bda9ade
cjdango <cjdango@gmail.com>
2019-04-01 15:01:25 +0800

    Comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,39 @@
+def is_equal(a, b):
+    """
+    Compares a and b, returns True if equal and False if not.
+    """
+    return a == b
+
+def not_equal(a, b):
+    """
+    Compares a and b, returns False if equal and True if not.
+    """
+    return a != b
+
+def left_greater(a, b):
+    """
+    If the value of left operand is greater than the value of 
+    right operand, returns True. False if not.
+    """
+    return a > b
+
+def left_lesser(a, b):
+    """
+    If the value of left operand is lesser than the value of 
+    right operand, returns True. False if not.
+    """
+    return a < b
+
+def left_greater_equal(a, b):
+    """
+    If the value of left operand is greater than or equal to the value of 
+    right operand, returns True. False if not.
+    """
+    return a >= b
+
+def left_lesser_equal(a, b):
+    """
+    If the value of left operand is lesser than or equal to the value of 
+    right operand, returns True. False if not.
+    """
+    return a <= b
--- main.py
+++ main.py
@@ -10,3 +10,13 @@ if __name__ == '__main__':
     print('get_remainder(15, 4) =', arithmetic_operators.get_remainder(15, 4))
     print('pow(4, 4) =', arithmetic_operators.pow(4, 4))
     print('floor_div(9, 2) =', arithmetic_operators.floor_div(9, 2))
+
+    print('\n###  Comparison Operators ###')
+    print('is_equal(3, 3) =', comparison_operators.is_equal(3, 3))
+    print('not_equal(3, 1) =', comparison_operators.not_equal(3, 1))
+    print('left_greater(3, 1) =', comparison_operators.left_greater(3, 1))
+    print('left_lesser(1, 5) =', comparison_operators.left_lesser(1, 5))
+    print('left_greater_equal(3, 6) =', comparison_operators.left_greater_equal(3, 6))
+    print('left_greater_equal(6, 6) =', comparison_operators.left_greater_equal(6, 6))
+    print('left_lesser_equal(3, 6) =', comparison_operators.left_lesser_equal(3, 6))
+    print('left_lesser_equal(6, 6) =', comparison_operators.left_lesser_equal(6, 6))
