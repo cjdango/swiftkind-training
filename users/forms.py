92f92c4fb079a34c6d28b09bb7805a5f29030dc0
cjdango <cjdango@gmail.com>
2019-04-09 13:43:25 +0800

    Add first name and last name in signup form.

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,16 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
+class SignupForm(forms.Form):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=150)
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+
+
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
