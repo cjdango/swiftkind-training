6a3ae68796cd531a5893d5409dcafe3aa0a25fca
cjdango <cjdango@gmail.com>
2019-04-09 11:02:35 +0800

    Add style to boards.

--- boards/forms.py
+++ boards/forms.py
@@ -4,6 +4,15 @@ from .models import Board, List, Card
 
 
 class CreateBoardForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Card title here...'
+        })
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(CreateBoardForm, self).__init__(*args, **kwargs)
+        self.fields['title'].label = ''
 
     class Meta:
         model = Board
@@ -22,3 +31,4 @@ class CreateCardForm(forms.ModelForm):
     class Meta:
         model = Card
         fields = ('title',)
+        form_show_labels = False
